f615f8c92bdd16b2dd7d44ce72324bc7f8a6400e
Kulkarnisumedha <94223551+Kulkarnisumedha@users.noreply.github.com>
2021-11-25 18:33:22 +0530

    Add files via upload

---
+++ switch_case.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+
+int main()
+
+{
+int day;
+cout<<"Enter values for days 1 to 7:";
+cin>>day;
+switch (day){
+case 1:
+cout<<"Monday";
+break;
+case 2:
+cout<<"Tuesday";
+break;
+case 3:
+cout<<"wednesday";
+break;
+case 4:
+cout<<"thursday";
+break;
+case 5:
+cout<<"friday";
+break;
+case 6:
+cout<<"saturday";
+break;
+case 7:
+cout<<"sunday";
+break;
+}
+return 0;
+}
